088e856c2237403c432914f7cb8334542121c879
xm <17723233960@163.com>
2026-06-15 21:26:02 +0800

    由LaerStack内部自动触发事件

--- Hazel/src/hazel/LayerStack.cpp
+++ Hazel/src/hazel/LayerStack.cpp
@@ -13,11 +13,13 @@ LayerStack::~LayerStack() {
 
 void LayerStack::pushLayer(Layer* layer) {
 	layers_.emplace(layers_.begin() + layer_insert_index_, layer);
+	layer->onAttach();
 	layer_insert_index_++;
 }
 
 void LayerStack::pushOverlay(Layer* overlay) {
 	layers_.emplace_back(overlay);
+	overlay->onAttach();
 }
 
 void LayerStack::popLayer(Layer* layer) {
@@ -25,6 +27,7 @@ void LayerStack::popLayer(Layer* layer) {
 	if (it != layers_.end()) {
 		layers_.erase(it);
 		layer_insert_index_--;
+		layer->onDetach();
 	}
 }
 
@@ -32,6 +35,7 @@ void LayerStack::popOverlay(Layer* overlay) {
 	auto it = std::ranges::find(layers_, overlay);
 	if (it != layers_.end()) {
 		layers_.erase(it);
+		overlay->onDetach();
 	}
 }
 }  // namespace hazel
