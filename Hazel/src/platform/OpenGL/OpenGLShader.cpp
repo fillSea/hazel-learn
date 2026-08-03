b0cb36b1e126e7c1e34dfdc49d326db4ec706709
xm <17723233960@163.com>
2026-08-03 21:34:25 +0800

    文件读检查

--- Hazel/src/platform/OpenGL/OpenGLShader.cpp
+++ Hazel/src/platform/OpenGL/OpenGLShader.cpp
@@ -47,10 +47,15 @@ std::string OpenGLShader::readFile(const std::string& filepath) {
 	std::ifstream in(filepath, std::ios::in | std::ios::binary);
 	if (in) {
 		in.seekg(0, std::ios::end);
-		result.resize(in.tellg());
-		in.seekg(0, std::ios::beg);
-		in.read(result.data(), result.size());
-		in.close();
+		size_t size = in.tellg();
+		if (size != -1) {
+			result.resize(size);
+			in.seekg(0, std::ios::beg);
+			in.read(result.data(), size);
+			in.close();
+		} else {
+			HZ_CORE_ERROR("Could not read from file '{0}'", filepath);
+		}
 	} else {
 		HZ_CORE_ERROR("Could not open file '{0}'", filepath);
 	}
