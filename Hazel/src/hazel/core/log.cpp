4adb804a3b6525be25889da8b34a81c2479a73e5
xm <17723233960@163.com>
2026-08-06 23:26:59 +0800

    日志消息现在额外发送到Hazel.log文件

--- Hazel/src/hazel/core/log.cpp
+++ Hazel/src/hazel/core/log.cpp
@@ -1,5 +1,7 @@
 #include "Log.h"
 
+#include <spdlog/sinks/basic_file_sink.h>
+
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
 
@@ -9,11 +11,22 @@ std::shared_ptr<spdlog::logger> Log::client_logger_;
 
 void Log::init() {
 	// 设置日志格式: [时间] 日志级别 日志名称: 日志消息
-	spdlog::set_pattern("%^[%T] %n: %v%$");
-	core_logger_ = spdlog::stdout_color_mt("Hazel");
-	core_logger_->set_level(spdlog::level::trace);
+	std::vector<spdlog::sink_ptr> log_sinks;
+	log_sinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	log_sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
 
-	client_logger_ = spdlog::stdout_color_mt("APP");
+	log_sinks[0]->set_pattern("%^[%T] %n: %v%$");
+	log_sinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+	core_logger_ = std::make_shared<spdlog::logger>("HAZEL", begin(log_sinks), end(log_sinks));
+	spdlog::register_logger(core_logger_);
+
+	core_logger_->set_level(spdlog::level::trace);
+	core_logger_->flush_on(spdlog::level::trace);
+
+	client_logger_ = std::make_shared<spdlog::logger>("APP", begin(log_sinks), end(log_sinks));
+	spdlog::register_logger(client_logger_);
 	client_logger_->set_level(spdlog::level::trace);
+	client_logger_->flush_on(spdlog::level::trace);
 }
 }  // namespace hazel
